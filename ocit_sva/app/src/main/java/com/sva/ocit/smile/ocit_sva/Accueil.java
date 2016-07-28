c7d3eb24a248c2f0fe9824c15b646390e53b329c
adino <noel.adingra@smile.ci>
2016-07-28 18:13:37 +0100

    commmit

---
+++ ocit_sva/app/src/main/java/com/sva/ocit/smile/ocit_sva/Accueil.java
@@ -0,0 +1,13 @@
+package com.sva.ocit.smile.ocit_sva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+}
